b8d919e35b4c6f929f1a1d198efa7dd5f2700719
Wjinlei <freebufer.wang@gmail.com>
2023-04-03 15:52:39 +0800

    refactor: Optimize

--- Driver.Test/DriverTest.cs
+++ Driver.Test/DriverTest.cs
@@ -24,10 +24,10 @@ public class Tests
         iis.Sites["www.test1.com"]?.Bindings.Add("m.test1.com", 80);
 
         // Delete site example
-        var test2 = iis.Sites["www.test1.com"];
-        Assert.That(test2, Is.Not.Null);
-        iis.Sites.Remove(test2);
-        iis.Sites.Remove("www.test3.com"); // It can also be removed by passing the website name
+        //var test1 = iis.Sites["www.test1.com"];
+        //Assert.That(test1, Is.Not.Null);
+        //iis.Sites.Remove(test1);
+        //iis.Sites.Remove("www.test2.com"); // It can also be removed by passing the website name
 
         // Modify site binding information
         var binding = iis.Sites["www.test1.com"]?.Bindings["*:80:m.test1.com"];
@@ -35,9 +35,9 @@ public class Tests
         binding.Port = 8080;
 
         // Delete binding information
-        iis.Sites["www.test1.com"]?.Bindings.Remove(binding);
         iis.Sites["www.test1.com"]?.Bindings.Remove("*:8080:bbs.test1.com");
 
+        // Commit
         iis.CommitChanges();
     }
 }
